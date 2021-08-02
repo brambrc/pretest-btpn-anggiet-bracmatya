f39bb542a47aa0fefc957cb7f2a2128e425b851a
Anggiet Bracmatya <anggiet.bracmatya@gmail.com>
2021-08-02 09:02:41 +0700

    heroku port dynamic and procfile

--- server.js
+++ server.js
@@ -53,4 +53,4 @@ app.use(function(err, req, res, next) {
 
 
 
-app.listen(process.env.PORT, function(){ console.log('Node server listening on port 3000');});
+app.listen(process.env.PORT || 3000, function(){ console.log('Node server listening on port 3000');});
