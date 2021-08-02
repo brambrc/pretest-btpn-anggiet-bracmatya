0516660e006a908498459144fc4c7393486c7846
Anggiet Bracmatya <anggiet.bracmatya@gmail.com>
2021-08-02 07:56:21 +0700

    add heroku config2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://anggietBracmatya:Kmzway87a@@anggietbracmatya.6txdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://anggietBracmatya:Kmzway87aa@anggietbracmatya.6txdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
